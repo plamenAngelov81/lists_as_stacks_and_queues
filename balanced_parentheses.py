7e50586de7bcafc4c5b7c881d07965ee2220c552
Пламен Ангелов <103038521+pachobg@users.noreply.github.com>
2022-05-18 15:23:46 +0300

    Add files via upload

---
+++ balanced_parentheses.py
@@ -0,0 +1,28 @@
+class BalancedParentheses:
+    def __init__(self, parentheses):
+        self.parentheses = parentheses
+        self.balance_dict = {"(": ")", "{": "}", "[": "]"}
+        self.brackets = []
+
+    def process(self):
+        condition = True
+        for bracket in self.parentheses:
+            if bracket in "({[":
+                self.brackets.append(bracket)
+            elif not self.brackets:
+                condition = False
+                break
+            else:
+                last_open_bracket = self.brackets.pop()
+                if self.balance_dict[last_open_bracket] != bracket:
+                    condition = False
+                    break
+
+        if condition and len(self.brackets) == 0:
+            print("YES")
+        else:
+            print("NO")
+
+
+obj = BalancedParentheses(parentheses=input())
+obj.process()
